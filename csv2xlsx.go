41e74ed2da86123ea305401e1f2e41fb383cad1c
DerLinkshaender <mail@arminhanisch.de>
2018-06-21 00:33:16 +0200

    added better ParseFloat

--- csv2xlsx.go
+++ csv2xlsx.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -42,6 +43,35 @@ var (
 	versionInfo         string
 	tmpStr              string
 )
+
+// ParseFloat ist an advanced ParseFloat for golang, support scientific notation, comma separated number
+// from yyscamper at https://gist.github.com/yyscamper/5657c360fadd6701580f3c0bcca9f63a
+func ParseFloat(str string) (float64, error) {
+	val, err := strconv.ParseFloat(str, 64)
+	if err == nil {
+		return val, nil
+	}
+	//Some number may be seperated by comma, for example, 23,120,123, so remove the comma firstly
+	str = strings.Replace(str, ",", "", -1)
+	//Some number is specifed in scientific notation
+	pos := strings.IndexAny(str, "eE")
+	if pos < 0 {
+		return strconv.ParseFloat(str, 64)
+	}
+	var baseVal float64
+	var expVal int64
+	baseStr := str[0:pos]
+	baseVal, err = strconv.ParseFloat(baseStr, 64)
+	if err != nil {
+		return 0, err
+	}
+	expStr := str[(pos + 1):]
+	expVal, err = strconv.ParseInt(expStr, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return baseVal * math.Pow10(int(expVal)), nil
+}
 
 // parseCommaGroup parses a single comma group (x or x-y),
 // optionally followed by :datatype (used only for columns right now)
@@ -307,7 +337,7 @@ func writeCellContents(cell *xlsx.Cell, colString, colType string, rownum, colnu
 	case "text":
 		cell.SetString(colString)
 	case "number", "currency":
-		floatVal, err := strconv.ParseFloat(colString, 64)
+		floatVal, err := ParseFloat(colString)
 		if err != nil {
 			fmt.Println(fmt.Sprintf("Cell (%d,%d) is not a valid number, value: %s", rownum, colnum, colString))
 			success = false
